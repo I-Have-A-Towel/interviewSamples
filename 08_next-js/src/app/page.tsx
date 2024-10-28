cee5f540eb50ab8c2030e1dbd974c01aed30b77b
Ali Elias Lattice <eliasaali1@gmail.com>
2024-10-28 10:19:46 -0600

    next sample

---
+++ 08_next-js/src/app/page.tsx
@@ -0,0 +1,13 @@
+//TASK 1:
+//TODO: Create a new component called "Header"
+//TODO: Add the Header component to the App component
+//TODO: Do your best to match the styling of https://vite.dev/
+//      Feel free to use any styling method you'd like, we have CSS, SCSS and tailwind installed
+//      You can add additional libraries if you'd like, but it's not necessary
+//      You can use an icon library or just use lower case letters for the icons
+//      No need to make a functional component for the search bar or dropdowns
+//      Light or dark mode
+
+export default function Home() {
+  return <div className=""></div>;
+}
