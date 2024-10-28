225468e94c86839b537da55e86baa6de213732eb
Ali Elias Lattice <eliasaali1@gmail.com>
2024-10-28 10:06:54 -0600

    react question

---
+++ 07_React_Sample/src/App.tsx
@@ -0,0 +1,27 @@
+import "./App.css";
+import viteLogo from "/vite.svg";
+
+//TASK 1:
+//TODO: Create a new component called "Header"
+//TODO: Add the Header component to the App component
+//TODO: Do your best to match the styling of https://vite.dev/
+//      Feel free to use any styling method you'd like, we have CSS, SCSS and tailwind installed
+//      You can add additional libraries if you'd like, but it's not necessary
+//      You can use an icon library or just use lower case letters for the icons
+//      No need to make a functional component for the search bar or dropdowns
+//      Light or dark mode
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+    </>
+  );
+}
+
+export default App;
