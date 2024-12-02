99d7de8a3d4d41baf5510e12f504a0919ffe7fc8
Ali Elias Lattice <eliasaali1@gmail.com>
2024-12-02 10:00:51 -0700

    updated react interview with a better header

--- 07_React_Vite/src/App.tsx
+++ 07_React_Vite/src/App.tsx
@@ -4,7 +4,7 @@ import viteLogo from "/vite.svg";
 //TASK 1:
 //TODO: Create a new component called "Header"
 //TODO: Add the Header component to the App component
-//TODO: Do your best to match the styling of https://vite.dev/
+//TODO: Do your best to match the styling of src/images/Header-Sample.png
 //      Feel free to use any styling method you'd like, we have CSS, SCSS and tailwind installed
 //      You can add additional libraries if you'd like, but it's not necessary
 //      You can use an icon library or just use lower case letters for the icons
--- 08_Next_JS/src/app/page.tsx
+++ 08_Next_JS/src/app/page.tsx
@@ -1,7 +1,7 @@
 //TASK 1:
 //TODO: Create a new component called "Header"
 //TODO: Add the Header component to the App component
-//TODO: Do your best to match the styling of https://vite.dev/
+//TODO: Do your best to match the styling of the sample in src/images/Header-Sample.png
 //      Feel free to use any styling method you'd like, we have CSS, SCSS and tailwind installed
 //      You can add additional libraries if you'd like, but it's not necessary
 //      You can use an icon library or just use lower case letters for the icons
